eb9413b159d9cb12a61c2f8b25fac5a4bbf4bcfe
Zeshawn Khan <zeshawnk@gmail.com>
2018-09-10 06:49:01 +0000

    automatically backed up by learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,16 @@
+def instantiate_new_array
+    @my_new_array = Array.new
+end
+
+def array_with_two_elements
+  @my_two_array = ["hello", "goodbye"]
+end  
+
+def indexing
+@taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+end
+
+def first_element(@taylor_swift)
+ @taylor_swift.first
+
+end
